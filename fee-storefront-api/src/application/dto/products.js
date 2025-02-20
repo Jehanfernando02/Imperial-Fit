8ac7af4c44f22adfdc3c3027c4cbbae31b72ab9c
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-02-20 22:30:25 +0530

    Delete fee-storefront-api directory

--- fee-storefront-api/src/application/dto/products.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const createProductDto = z.object({
-  categoryId: z.string(),
-  image: z.string(),
-  name: z.string(),
-  price: z.number(),  // Changed from string to number
-  description: z.string(),
-});
