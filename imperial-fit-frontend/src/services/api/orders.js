705c25a4a9feb18f11c25e49c79353d33822a822
Jehan Fernando <119022648+Jehanfernando02@users.noreply.github.com>
2025-03-21 09:13:41 +0530

    updated url

--- imperial-fit-frontend/src/services/api/orders.js
+++ imperial-fit-frontend/src/services/api/orders.js
@@ -1,7 +1,7 @@
 export const createOrder = async (data) => {
   const token = await window.Clerk?.session?.getToken();
 
-  const res = await fetch("http://localhost:8000/api/orders", {
+  const res = await fetch("https://imperial-fit-61ce.vercel.app/api/orders", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -16,7 +16,7 @@ export const createOrder = async (data) => {
 export const getOrderById = async (id) => {
   const token = await window.Clerk?.session?.getToken();
 
-  const res = await fetch(`http://localhost:8000/api/orders/${id}`, {
+  const res = await fetch(`https://imperial-fit-61ce.vercel.app/api/orders/${id}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -33,7 +33,7 @@ export const getOrderById = async (id) => {
 export const getOrdersByUser = async (userId) => {
   const token = await window.Clerk?.session?.getToken();
 
-  const res = await fetch(`http://localhost:8000/api/orders/user/${userId}`, {
+  const res = await fetch(`https://imperial-fit-61ce.vercel.app/api/orders/user/${userId}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
--- imperial-fit-frontend/src/services/api/products.js
+++ imperial-fit-frontend/src/services/api/products.js
@@ -2,7 +2,7 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
 
 export const getProductById = async (id) => {
   try {
-    const res = await fetch(`http://localhost:8000/api/products/${id}`, {
+    const res = await fetch(`https://imperial-fit-61ce.vercel.app/api/products/${id}`, {
       method: "GET",
     });
 
@@ -29,7 +29,7 @@ export const getAllProducts = async ({ categoryId = "ALL", sort = "low-to-high" 
     }).toString();
 
 
-    const res = await fetch(`http://localhost:8000/api/products?${queryParams}`, {
+    const res = await fetch(`https://imperial-fit-61ce.vercel.app/api/products?${queryParams}`, {
       method: "GET",
     });
 
