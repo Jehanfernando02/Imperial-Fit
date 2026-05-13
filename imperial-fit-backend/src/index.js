4cefa106f5a12656a807a6d5a94b2c76069c98f0
Jehanfernando02 <02jehanfernando@gmail.com>
2026-05-13 13:05:09 +0530

    Updated packages

--- imperial-fit-backend/src/index.js
+++ imperial-fit-backend/src/index.js
@@ -11,7 +11,7 @@ import { connectDB } from "./infrastructure/db.js";
 import { globalErrorHandler } from "./api/middleware/global-error-handler.js";
 import cors from "cors";
 
-
+console.log("MONGO_URI =", process.env.MONGO_URI);
 const app = express();
 
 // Use the webhook router BEFORE express.json()
